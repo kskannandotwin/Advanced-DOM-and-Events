09ce4796cd8f00fd07f3174ec4a32fb3cabf937c
kannanATexcelforte <kannan@excelforte.com>
2024-04-11 17:16:04 +0530

    11.04.2024-iss
    completed

--- script.js
+++ script.js
@@ -34,81 +34,27 @@ document.addEventListener('keydown', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 
-// selecting elements
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+btnScrollTo.addEventListener('click', function (e) {
+    const s1coords = section1.getBoundingClientRect();
+    console.log(s1coords);
 
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+    console.log(e.target.getBoundingClientRect());
 
-console.log(document.getElementsByClassName('btn'));
+    console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
 
-// creating and inserting elements
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-// message.textContent = 'We use cookies for improved functionality and analytics';
-message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close--cookie">Got It</button>';
+    console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
 
-// header.prepend(message);
-header.append(message);
-// header.append(message.cloneNode(true));
+    // scrolling
+    // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
 
-// header.before(message);
-// header.after(message);
+    // window.scrollTo({
+    //     left: s1coords.left + window.pageXOffset,
+    //     top: s1coords.top + window.pageYOffset,
+    //     behavior: "smooth"
+    // });
 
-// delete elements
-document.querySelector('.btn--close--cookie').addEventListener('click', function() {
-    // message.remove();
-    message.parentElement.removeChild(message);
+    section1.scrollIntoView({behavior: "smooth"});
 });
-
-// styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
-
-console.log(message.style.color);
-console.log(message.style.backgroundColor);
-
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
-
-message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
-
-document.documentElement.style.setProperty('--color-primary', 'orangered');
-
-// attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.className);
-
-logo.alt = 'Beautiful minimalist logo';
-
-// non-standard
-console.log(logo.designer);
-console.log(logo.getAttribute('designer'));
-logo.setAttribute('company', 'Bankist');
-
-console.log(logo.src);
-console.log(logo.getAttribute('src'));
-
-const link = document.querySelector('.nav__link--btn');
-console.log(link.href);
-console.log(link.getAttribute('href'));
-
-// data attributes
-console.log(logo.dataset.versionNumber);
-
-// classes
-logo.classList.add('c', 'j');
-logo.classList.remove('c', 'j');
-logo.classList.toggle('c');
-logo.classList.contains('c'); // not includes
-
-// don't use
-logo.className = 'jonas';
