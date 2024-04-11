49b76d79afbe7f547e82824c19c31a255917f7cc
kannanATexcelforte <kannan@excelforte.com>
2024-04-11 15:20:50 +0530

    11.04.2024-scade
    completed

--- script.js
+++ script.js
@@ -29,3 +29,41 @@ document.addEventListener('keydown', function (e) {
         closeModal();
     }
 });
+
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+// selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics';
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close--cookie">Got It</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// delete elements
+document.querySelector('.btn--close--cookie').addEventListener('click', function() {
+    // message.remove();
+    message.parentElement.removeChild(message);
+})
