198d4ac6b42f9b8d67492a5e99854dc8486ebfcc
kannanATexcelforte <kannan@excelforte.com>
2024-04-16 16:10:22 +0530

    16.04.2024-lli
    completed

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ tabsContainer.addEventListener('click', function (e) {
 
 // menu fade animation
 const handleHover = function (e) {
-  console.log(this, e.currentTarget);
+
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
@@ -153,11 +153,10 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+
   if (!entry.isIntersecting) nav.classList.add('sticky')
   else nav.classList.remove('sticky');
 }
@@ -174,7 +173,6 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
 
@@ -190,6 +188,33 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function() {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+});
+imgTargets.forEach(img => imgObserver.observe(img));
+
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
