20f0592ba35b130ede2f4a93ac88787b7debb31a
kannanATexcelforte <kannan@excelforte.com>
2024-04-16 11:43:01 +0530

    16.04.2024-iasntse
    completed

--- script.js
+++ script.js
@@ -128,5 +128,15 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+// sticky navigation
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', function() {
+  console.log(window.scrollY);
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky')
+  else nav.classList.remove('sticky');
+})
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
